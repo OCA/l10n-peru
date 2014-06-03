73acf5541e36bb684fa5ad20a3054df0d5fbb08f
Julio Serna <julio@vauxoo.com>
2014-06-02 20:05:53 -0500

    [ADD] added module that validated ruc or dni when limit amount exceeding

---
+++ l10n_pe_invoice/__openerp__.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#
+#    Copyright (c) 2012 Vauxoo - http://www.vauxoo.com
+#    All Rights Reserved.
+#    info@vauxoo.com
+############################################################################
+#    Coded by: Julio Serna (julio@vauxoo.com)
+############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name' : 'RUC and DNI Validation on Invoice',
+    'version' : '1.1',
+    'category': 'Invoice Management',
+    'depends' : ['base','account'],
+    'author' : 'Vauxoo',
+    'description': """
+Validation for invoice when exceeding minimum amount.
+=========================================================
+
+This module modifies the invoice workflow in order to validate RUC and DNI in Invoice that
+exceeds minimum amount set by configuration wizard.
+    """,
+    'website': 'http://www.openerp.com',
+    'data': [
+        'workflow/invoice_workflow.xml',
+        'view/invoice_conf_view.xml'
+    ],
+    'test': [
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
