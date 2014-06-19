2785fcd62ca51f7f93eafc3e3c570a513da6afc8
Juan Carlos Funes <juan@vauxoo.com>
2014-06-19 18:34:17 -0500

    [ADD][l10n_pe_base_vat_split]add l10n_pe_base_vat_split module

---
+++ l10n_pe_base_vat_split/model/base_vat.py
@@ -0,0 +1,47 @@
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#
+#    Copyright (c) 2010 Vauxoo - http://www.vauxoo.com/
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+############################################################################
+#    Coded by: vauxoo consultores (info@vauxoo.com)
+############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp.tools.translate import _
+from openerp.osv import fields, osv
+
+
+class res_partner(osv.Model):
+    _inherit = 'res.partner'
+
+    def _get_base_vat_split(self, cr, uid, ids, field_names=None, arg=False, context=None):
+        if context is None:
+            context = {}
+        res = {}
+        for partner in self.browse(cr, uid, ids, context=context):
+            res[partner.id] = partner.vat and partner.vat[2:] or False
+        return res
+
+    _columns = {
+        'vat_split': fields.function(_get_base_vat_split, method=True,
+                    type='char', size=64, string='VAT Split', store=True,
+                    help='Remove the prefix of the country of the VAT'),
+    }
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
