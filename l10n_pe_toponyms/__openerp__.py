5f627522ff6914727681b04e72c62ebf1a5ca1c5
julioserna <julio@vauxoo.com>
2014-09-09 19:26:11 -0500

    [PEP8][l10n_pe_toponomys]

--- l10n_pe_toponyms/__openerp__.py
+++ l10n_pe_toponyms/__openerp__.py
@@ -30,7 +30,8 @@
     'depends': ['base'],
     'author': 'Vauxoo',
     'description': """
-    This module add the fields that require the address of partner in the l10n_pe
+        This module add the fields that require the address of partner in the
+        l10n_pe.
     """,
     "website": "http://www.vauxoo.com/",
     'data': [
