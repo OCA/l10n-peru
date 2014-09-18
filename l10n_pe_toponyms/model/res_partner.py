5f9cdc2110b1225757dcfba587c5c86d73a1298e
jcfunes <juan@vauxoo.com>
2014-09-18 17:28:35 -0500

    [FIX][l10n_pe_toponyms]fix iteration with columns

--- l10n_pe_toponyms/model/res_partner.py
+++ l10n_pe_toponyms/model/res_partner.py
@@ -48,8 +48,7 @@ class res_partner(osv.osv):
             'res.country')
         ids = country_obj.search(
             cr, uid, [('code', '=', 'PE'), ], limit=1)
-        ids = ids and ids[
-            0] or False
+        ids = ids and ids[0] or False
         return ids
 
     def fields_view_get_address(self, cr, uid, arch, context=None):
@@ -80,7 +79,7 @@ class res_partner(osv.osv):
         fmt = fmt and fmt.address_format
         city = '<field name="city" placeholder="City..." style="width:\
                     40%%" modifiers="{&quot;invisible&quot;: true}"/>'
-        for name in self._columns.items():
+        for name in self._columns.keys():
             if name == 'city_id':
                 city = '<field name="city" \
                         modifiers="{&quot;invisible&quot;: true}" \
